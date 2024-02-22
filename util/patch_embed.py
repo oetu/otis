1d5302904b8eb83854adeb543095246d7c8a7445
Oezguen Turgut <oezguen.turgut@tum.de>
2024-02-22 10:58:22 +0100

    Update: apply LayerNorm and GELU for patch embedding

--- util/patch_embed.py
+++ util/patch_embed.py
@@ -15,21 +15,31 @@ class PatchEmbed(nn.Module):
     """
     def __init__(self, input_channels=1, patch_size=(1, 100), embed_dim=192, norm_layer=None, flatten=True):
         super().__init__()
-        # self.input_size = input_size
-        # self.patch_size = patch_size
-        # self.grid_size = (input_size[1] // patch_size[0], input_size[2] // patch_size[1])
-        # self.num_patches = self.grid_size[0] * self.grid_size[1]
         self.flatten = flatten
 
         self.proj = nn.Conv2d(input_channels, embed_dim, kernel_size=patch_size, stride=patch_size)
-        self.norm = norm_layer(embed_dim) if norm_layer else nn.Identity()
+        self.norm = nn.LayerNorm(embed_dim)
+        self.act_ft = nn.GELU()
+        
+        # self.norm = norm_layer(embed_dim) if norm_layer else nn.Identity()
 
     def forward(self, x):
-        # B, C, H, W = x.shape
-        # assert H == self.input_size[1], f"Input image height ({H}) doesn't match model ({self.input_size[1]})."
-        # assert W == self.input_size[2], f"Input image width ({W}) doesn't match model ({self.input_size[2]})."
+        """
+        x: (B, C, H, W)
+        """
+        # (B, D, H', W')
         x = self.proj(x)
-        if self.flatten:
-            x = x.flatten(2).transpose(1, 2)  # BDH'W' -> BND
+
+        # (B, H', W', D)
+        x = x.permute(0, 2, 3, 1)
         x = self.norm(x)
+        x = self.act_ft(x)
+
+        # (B, D, H', W')
+        x = x.permute(0, 3, 1, 2)
+
+        if self.flatten:
+            # (B, N, D)
+            x = x.flatten(2).transpose(1, 2)  # BDH'W' -> BND
+
         return x
