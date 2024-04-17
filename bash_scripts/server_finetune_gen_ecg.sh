76f2e1bf4c1b22b9831bdebbd58e2da7e07a8fb6
Oezguen Turgut <oezguen.turgut@tum.de>
2024-04-17 16:57:07 +0200

    Update: bash scripts

--- bash_scripts/server_finetune_gen_ecg.sh
+++ bash_scripts/server_finetune_gen_ecg.sh
@@ -5,10 +5,10 @@
 #SBATCH --output=/vol/aimspace/users/tuo/SiT/slurm_output/fin/ecg-%A.out    # Standard output of the script (Can be absolute or relative path). %A adds the job id to the file name so you can launch the same script multiple times and get different logging files
 #SBATCH --error=/vol/aimspace/users/tuo/SiT/slurm_output/fin/ecg-%A.err     # Standard error of the script
 #SBATCH --time=4-23:59:59   # Limit on the total run time (format: days-hours:minutes:seconds)
-#SBATCH --gres=gpu:4        # Number of GPUs if needed
+#SBATCH --gres=gpu:1        # Number of GPUs if needed
 #SBATCH --gpu-bind=none     # NCCL can't deal with task-binding (binding each process to its own GOU), hence allocate the devices manually in the python script
 #SBATCH --cpus-per-task=24  # Number of CPUs (Don't use more than 24 per GPU)
-#SBATCH --mem=256G          # Memory in GB (Don't use more than 126G per GPU)
+#SBATCH --mem=32G          # Memory in GB (Don't use more than 126G per GPU)
 
 
 # load python module
