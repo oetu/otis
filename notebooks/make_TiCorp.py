1b3c7e83526a4f4b5e1b9e4382fbf57401fdf4ab
Oezguen Turgut <oezguen.turgut@tum.de>
2024-05-10 14:35:51 +0200

    Update: ticorp script

--- notebooks/make_TiCorp.py
+++ notebooks/make_TiCorp.py
@@ -15,11 +15,11 @@ filename = "/vol/aimspace/projects/physionet/mimic/processed/mimic-ecg-text/ecgs
 ecg_data = np.memmap(filename, dtype='float32', mode='r+', shape=(800035, 12, 5000))
 
 print("Load remaining data\n")
-# other_train_path = f"/vol/aimspace/users/tuo/data/processed/TiCorp/train_wo_mimic.pt"
-# other_train = torch.load(other_train_path)
+other_train_path = f"/vol/aimspace/users/tuo/data/processed/TiCorp/train_wo_mimic_new.pt"
+other_train = torch.load(other_train_path)
 
-other_val_path = f"/vol/aimspace/users/tuo/data/processed/TiCorp/val_wo_mimic.pt"
-other_vaĺ = torch.load(other_val_path)
+other_val_path = f"/vol/aimspace/users/tuo/data/processed/TiCorp/val_wo_mimic_new.pt"
+other_val = torch.load(other_val_path)
 
 
 def create_ticorp(idx):
@@ -34,8 +34,8 @@ if __name__ == '__main__':
     num_processes = 32
 
     lower_bnd = 0
-    upper_bnd = 800035
-    
+    upper_bnd = 400000 # 800035
+
     # Create a multiprocessing Pool
     print("PROCESS DATA")
     with multiprocessing.Pool(processes=num_processes) as pool:
@@ -43,17 +43,19 @@ if __name__ == '__main__':
 
     print(f"Number of processed data points: {len(ecgs_mimic)}")
 
-    # ecgs_mimic_train = [("ecg", torch.from_numpy(sample)) for sample in ecgs_mimic[:int(0.9875*upper_bnd)]]
-    # ticorp_train = ecgs_mimic_train + other_train
+    ecgs_mimic_train = [("ecg", torch.from_numpy(sample)) for sample in ecgs_mimic[:int(0.975*upper_bnd)]]
+    # ecgs_mimic_train = [("ecg", torch.from_numpy(sample)) for sample in ecgs_mimic]
+    ticorp_train = ecgs_mimic_train + other_train
 
-    ecgs_mimic_val = [("ecg", torch.from_numpy(sample)) for sample in ecgs_mimic[int(0.9875*upper_bnd):]]
-    ticorp_val = ecgs_mimic_val + other_vaĺ
+    ecgs_mimic_val = [("ecg", torch.from_numpy(sample)) for sample in ecgs_mimic[int(0.975*upper_bnd):]]
+    # ecgs_mimic_val = [("ecg", torch.from_numpy(sample)) for sample in ecgs_mimic]
+    ticorp_val = ecgs_mimic_val + other_val
 
     # Save data
     output_path = f"/vol/aimspace/users/tuo/data/processed/TiCorp"
     print(f"SAVE DATA ({output_path})")
-    # torch.save(ticorp_train, f"{output_path}/train_all.pt", pickle_protocol=4)
-    torch.save(ticorp_val, f"{output_path}/val_all.pt", pickle_protocol=4)
+    # torch.save(ticorp_train, f"{output_path}/train_all_new.pt", pickle_protocol=4)
+    # torch.save(ticorp_val, f"{output_path}/val_all_new.pt", pickle_protocol=4)
 
     total_time = time.time() - start_time
     print(f"Processing time: {total_time}\n")
