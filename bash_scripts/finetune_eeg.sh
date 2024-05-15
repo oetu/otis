a5c87569ef881374afc6e027af244d3fa89ae333
Oezguen Turgut <oezguen.turgut@tum.de>
2024-05-15 15:33:03 +0200

    Update: bash script

--- bash_scripts/finetune_eeg.sh
+++ bash_scripts/finetune_eeg.sh
@@ -3,26 +3,48 @@
 
 # Basic parameters seed = [0, 101, 202, 303, 404]
 seed=(0)
+num_workers="24"
+
+path="server"       # [tower, server]
+submitit="False"    # only for training on server
+
+nodes="1"
+world_size="4"      # number of GPUs
+mem_per_task="96"   # memory per GPU
+port="29450"
+
 batch_size=(32)
 accum_iter=(1)
 
-epochs="200"
-warmup_epochs="20"
+epochs="100"
+warmup_epochs="10"
 
 # Callback parameters
 patience="15"
 max_delta="0.15" # for AUROC
 
 # Model parameters
-input_channels="1"
-input_electrodes="30"
-time_steps="6000"
-
-model_size="tinyDeep"
+model_size="baseDeep"
 model="vit_"$model_size"_patchX"
 
+univariate="False"
+
+# Pretraining specifications
+from_scratch="False"
+
+ignore_pos_embed_y="False"
+freeze_pos_embed_y="False"
+
+input_channels="1"
+input_electrodes="30"
+time_steps="1008"
+
 patch_height="1"
-patch_width=(100)
+patch_width=(24)
+
+# Pooling strategy
+global_pool=(True)
+attention_pool=(False)
 
 # Augmentation parameters
 masking_blockwise="False"
@@ -48,25 +70,41 @@ weight_decay=(0.1)
 # Criterion parameters
 smoothing=(0.1)
 
-folds=(0 1 2 3 4 5 6 7 8 9)
+eval_criterion="rmse"
+
+# Output path
+folder="Age"
+
+# Log specifications
+save_output="True"
+wandb="True"
+wandb_entity="oturgut"
+wandb_project="OTIS_Age_Regression"
+wandb_id=""
+
+plot_attention_map="False"
+plot_embeddings="False"
+save_embeddings="False"
+save_logits="True"
+
+folds=(0) # 1 2 3 4 5 6 7 8 9)
 for fold in "${folds[@]}"
 do
 
     # Data path
     path="server"
     if [ "$path" = "tower" ]; then
-        data_base="/home/oturgut/data/processed/lemon/kfold/fold"$fold
+        data_base="/home/oturgut/data/processed/LEMON/kfold/fold"$fold
         checkpoint_base="/home/oturgut/SiT"
     else
-        data_base="/vol/aimspace/users/tuo/data/lemon/kfold/fold"$fold
+        data_base="/vol/aimspace/users/tuo/data/processed/LEMON/kfold/fold"$fold
         checkpoint_base="/vol/aimspace/users/tuo/SiT"
     fi
 
     # Dataset parameters
     # Training
-    data_path=$data_base"/data_train.pt"
+    data_path=$data_base"/data_train_otis.pt"
     labels_path=$data_base"/labels_train_stdNormed.pt"
-    # labels_mask_path=$data_base"/labels_train_Regression_mask.pt"
     downstream_task="regression"
     # Age
     lower_bnd="0"
@@ -74,36 +112,16 @@ do
     nb_classes="1"
 
     # Validation 
-    val_data_path=$data_base"/data_val.pt"
+    val_data_path=$data_base"/data_val_otis.pt"
     val_labels_path=$data_base"/labels_val_stdNormed.pt"
-    # val_labels_mask_path=$data_base"/labels_val_Regression_mask.pt"
-
-    global_pool=(False)
-    attention_pool=(True)
-    num_workers="24"
-
-    # Log specifications
-    save_output="True"
-    wandb="True"
-    wandb_project="MAE_EEG_Age"
-    wandb_id=""
-
-    plot_attention_map="False"
-    plot_embeddings="False"
-    save_embeddings="False"
-    save_logits="True"
-
-    # Pretraining specifications
-    ignore_pos_embed_y="False"
-    freeze_pos_embed_y="False"
-
-    from_scratch="False"
 
     # EVALUATE
-    eval="False"
     # As filename: State the checkpoint for the inference of a specific model
     # or state the (final) epoch for the inference of all models up to this epoch
-    #resume=$checkpoint_base"/output/fin/"$folder"/id/"$subfolder"/fin_b"$(($batch_size*$accum_iter))"_blr"$blr"/checkpoint-89.pth"
+    # eval_ckpt="checkpoint-253-avg-100.0000.pth"
+    # blr=(3e-3)
+    # val_data_path=$data_base"/test.pt"
+    # val_labels_path=$data_base"/test_labels.pt"
 
     for sd in "${seed[@]}"
     do
@@ -122,9 +140,19 @@ do
                             for smth in "${smoothing[@]}"
                             do
 
-                                folder="lemon/kfold/fold"$fold"/eeg/Age/SiT"
-                                subfolder="seed$sd/"$model_size"/t"$time_steps"/p"$patch_height"x"$patch_width"/ld"$ld"/dp"$dp"/smth"$smth"/wd"$weight_decay"/m0.8"
+                                folder=$folder"/fold"$fold
+                                subfolder="seed$sd/"$model_size"/t"$time_steps"/p"$patch_height"x"$patch_width"/ld"$ld"/dp"$dp"/smth"$smth"/wd"$wd
                                 
+                                # OTiS 
+                                if [ "$model_size" = "baseDeep" ]; then
+                                    finetune="/vol/aimspace/users/tuo/SiT/output/pre/otis/ticorp/cos_weight0.0/ncc_weight0.1/seed0/baseDeep_dec160d4b/t1008/p1x24/wd0.15/m0.75/pre_b2624_blr3e-5/checkpoint-99-ncc-0.8685.pth"
+                                elif [ "$model_size" = "largeDeep" ]; then
+                                    finetune="/vol/aimspace/users/tuo/SiT/output/pre/otis/ticorp/cos_weight0.0/ncc_weight0.1/seed0/largeDeep_dec160d4b/t1008/p1x24/wd0.15/m0.75/pre_b768_blr3e-5/checkpoint-96-ncc-0.8667.pth"
+                                else
+                                    # huge
+                                    finetune="/vol/aimspace/users/tuo/SiT/output/pre/otis/ticorp/cos_weight0.0/ncc_weight0.1/seed0/hugeDeep_dec160d4b/t1008/p1x24/wd0.15/m0.75/pre_b1680_blr1e-5/checkpoint-98-ncc-0.8661.pth"
+                                fi
+
                                 # finetune=$checkpoint_base"/output/pre/tuh/eeg/all/15ch/ncc_weight0.1/seed0/tiny/t3000/p1x100/wd0.15/m0.8/pre_b256_blr1e-5/checkpoint-196-ncc-0.78.pth"
 
                                 # finetune="/vol/aimspace/users/tuo/SiT/output/pre/TempEncoder/cos_weight0.0/ncc_weight0.1/seed0/tinyDeep2/t2500/p1x100/wd0.15/m0.8/pre_b128_blr3e-5/checkpoint-173-ncc-0.8602.pth"
@@ -135,34 +163,26 @@ do
                                 # finetune="/vol/aimspace/users/tuo/SiT/output/pre/fresh/noTempEncoder/WLoss/cos_weight0.0/ncc_weight0.1/seed0/tinyDeep2/t2500/p1x100/wd0.15/m0.8/pre_b768_blr1e-5/checkpoint-198-ncc-0.8803.pth"
                                 # finetune="/vol/aimspace/users/tuo/SiT/output/pre/fresh/noTempEncoder/noWLoss/cos_weight0.0/ncc_weight0.1/seed0/tinyDeep2/t2500/p1x100/wd0.15/m0.8/pre_b768_blr1e-5/checkpoint-198-ncc-0.9177.pth"
 
-                                finetune="/vol/aimspace/users/tuo/SiT/output/pre/fresh/WLoss/NewRandomResizedCrop/cos_weight0.0/ncc_weight0.1/seed0/tinyDeep2/t6000/p1x100/wd0.15/m0.8/pre_b320_blr1e-5/checkpoint-199-ncc-0.8799.pth"
-                                
-                                # finetune="/vol/aimspace/users/tuo/SiT/output/pre/fresh/WLoss/randomResizedCrop/cos_weight0.0/ncc_weight0.1/seed0/tinyDeep2/t6000/p1x100/wd0.15/m0.8/pre_b320_blr1e-5/checkpoint-199-ncc-0.8731.pth"
-                                # finetune="/vol/aimspace/users/tuo/SiT/output/pre/fresh/WLoss/cos_weight0.0/ncc_weight0.1/seed0/tinyDeep2/t6000/p1x100/wd0.15/m0.8/pre_b320_blr1e-5/checkpoint-199-ncc-0.8900.pth"
+                                # finetune="/vol/aimspace/users/tuo/SiT/output/pre/fresh/WLoss/NewRandomResizedCrop/cos_weight0.0/ncc_weight0.1/seed0/tinyDeep2/t6000/p1x100/wd0.15/m0.8/pre_b320_blr1e-5/checkpoint-199-ncc-0.8799.pth"
 
-                                # finetune="/vol/aimspace/users/tuo/SiT/output/pre/fresh/WLoss/cos_weight0.0/ncc_weight0.1/seed0/tinyDeep2/t2500/p1x100/wd0.15/m0.8/pre_b768_blr1e-5/checkpoint-198-ncc-0.8827.pth"
-                                # finetune="/vol/aimspace/users/tuo/SiT/output/pre/fresh/noWLoss/cos_weight0.0/ncc_weight0.1/seed0/tinyDeep2/t2500/p1x100/wd0.15/m0.8/pre_b768_blr1e-5/checkpoint-198-ncc-0.9158.pth"
-
-                                # finetune="/home/oturgut/SiT/output/pre/noWLoss/cos_weight0.0/ncc_weight0.1/seed0/tinyDeep2/t2500/p1x100/wd0.15/m0.8/pre_b128_blr3e-5/checkpoint-98-ncc-0.8766.pth"
-                                # finetune="/home/oturgut/SiT/output/pre/TempEncoder/cos_weight0.0/ncc_weight0.1/seed0/tinyDeep/t2500/p1x100/wd0.15/m0.8/pre_b576_blr1e-5/checkpoint-198-ncc-0.9233.pth"
-                                # finetune="/home/oturgut/SiT/output/pre/wLoss/cos_weight0.0/ncc_weight0.1/seed0/tinyDeep2/t5000/p1x100/wd0.15/m0.8/pre_b128_blr3e-5/checkpoint-242-ncc-0.8882.pth"
-                                # finetune="/home/oturgut/SiT/output/pre/cos_weight0.0/ncc_weight0.1/seed0/tinyDeep/t2500/p1x100/wd0.15/m0.8/pre_b576_blr1e-5/checkpoint-199-ncc-0.8946.pth"
-                                # finetune="/home/oturgut/SiT/output/pre/test4/cos_weight0.0/ncc_weight0.1/seed0/tinyDeep2/t5000/p1x100/wd0.15/m0.8/pre_b128_blr3e-5/checkpoint-280-ncc-0.6527.pth"
-
-                                # finetune="/home/oturgut/mae/output/pre/siggy/cos_weight0.1/ncc_weight0.1/seed0/tinyDeep2/t2500/p1x100/wd0.15/m0.8/pre_b1024_blr1e-5/checkpoint-399-ncc-0.8839.pth"
-
-                                # finetune="/home/oturgut/mae/output/pre/tuh/250Hz/eeg/10ch/ncc_weight0.1/seed0/tinyUp/t3000/p1x100/wd0.15/m0.8/pre_b256_blr1e-5/checkpoint-199-ncc-0.8074.pth"
-                                # finetune="/home/oturgut/mae/output/pre/tuh/250Hz/eeg/ncc_weight0.1/seed0/tiny/t3000/p1x100/wd0.15/m0.8/pre_b256_blr1e-5/checkpoint-199-ncc-0.8500.pth"
-                                # finetune="/home/oturgut/mae/checkpoints/mm_v230_mae_checkpoint.pth"
-                                # finetune="/home/oturgut/mae/checkpoints/tiny/v1/checkpoint-399.pth"
-                                # finetune="/home/oturgut/mae/output/pre/lemon/full/eeg/ncc_weight0.1/seed0/tiny/t6000/p1x100/wd0.15/m0.8/pre_b64_blr1e-4/checkpoint-196-ncc-0.81.pth"
-
-                                output_dir=$checkpoint_base"/output/fin/"$folder"/"$subfolder"/fin_b"$(($bs*$accum_iter))"_blr"$lr
+                                output_dir=$checkpoint_base"/output/fin/"$folder"/"$subfolder"/fin_b"$(($bs*$accum_iter*$world_size))"_blr"$lr
 
                                 # resume=$checkpoint_base"/output/fin/"$folder"/"$subfolder"/fin_b"$bs"_blr"$lr"/checkpoint-4-pcc-0.54.pth"
 
-                                cmd="python3 main_finetune.py --seed $sd --downstream_task $downstream_task --crop_lower_bnd $crop_lower_bnd --crop_upper_bnd $crop_upper_bnd --jitter_sigma $jitter_sigma --rescaling_sigma $rescaling_sigma --ft_surr_phase_noise $ft_surr_phase_noise --input_channels $input_channels --input_electrodes $input_electrodes --time_steps $time_steps --patch_height $patch_height --patch_width $patch_width --model $model --batch_size $bs --epochs $epochs --patience $patience --max_delta $max_delta --accum_iter $accum_iter --drop_path $dp --weight_decay $wd --layer_decay $ld --min_lr $min_lr --blr $lr --warmup_epoch $warmup_epochs --smoothing $smth --data_path $data_path --labels_path $labels_path --val_data_path $val_data_path --val_labels_path $val_labels_path --nb_classes $nb_classes --num_workers $num_workers"
-                                
+                                if [ "$path" = "tower" ]; then
+                                    cmd="python3 main_finetune.py --seed $sd --downstream_task $downstream_task --crop_lower_bnd $crop_lower_bnd --crop_upper_bnd $crop_upper_bnd --jitter_sigma $jitter_sigma --rescaling_sigma $rescaling_sigma --ft_surr_phase_noise $ft_surr_phase_noise --input_channels $input_channels --input_electrodes $input_electrodes --time_steps $time_steps --patch_height $patch_height --patch_width $patch_width --model $model --batch_size $bs --epochs $epochs --patience $patience --max_delta $max_delta --accum_iter $accum_iter --drop_path $dp --weight_decay $wd --layer_decay $ld --min_lr $min_lr --blr $lr --warmup_epochs $warmup_epochs --smoothing $smth --data_path $data_path --labels_path $labels_path --val_data_path $val_data_path --val_labels_path $val_labels_path --nb_classes $nb_classes --num_workers $num_workers"
+                                else
+                                    cmd="torchrun --rdzv-endpoint=localhost:$port --nproc_per_node $world_size --nnodes $nodes --node_rank 0 main_finetune.py --world_size $world_size --dist_eval --seed $sd --downstream_task $downstream_task --crop_lower_bnd $crop_lower_bnd --crop_upper_bnd $crop_upper_bnd --jitter_sigma $jitter_sigma --rescaling_sigma $rescaling_sigma --ft_surr_phase_noise $ft_surr_phase_noise --input_channels $input_channels --input_electrodes $input_electrodes --time_steps $time_steps --patch_height $patch_height --patch_width $patch_width --model $model --batch_size $bs --epochs $epochs --patience $patience --max_delta $max_delta --accum_iter $accum_iter --drop_path $dp --weight_decay $wd --layer_decay $ld --min_lr $min_lr --blr $lr --warmup_epochs $warmup_epochs --smoothing $smth --data_path $data_path --labels_path $labels_path --val_data_path $val_data_path --val_labels_path $val_labels_path --nb_classes $nb_classes --num_workers $num_workers"
+                                fi
+
+                                if [ "$univariate" = "True" ]; then
+                                    cmd=$cmd" --univariate"
+                                fi
+
+                                if [ ! -z "$eval_criterion" ]; then
+                                    cmd=$cmd" --eval_criterion $eval_criterion"
+                                fi
+
                                 if [ "$ignore_pos_embed_y" = "True" ]; then
                                     cmd=$cmd" --ignore_pos_embed_y"
                                 fi
@@ -204,7 +224,7 @@ do
                                 fi
 
                                 if [ "$wandb" = "True" ]; then
-                                    cmd=$cmd" --wandb --wandb_project $wandb_project"
+                                    cmd=$cmd" --wandb --wandb_entity $wandb_entity --wandb_project $wandb_project"
                                     if [ ! -z "$wandb_id" ]; then
                                         cmd=$cmd" --wandb_id $wandb_id"
                                     fi
@@ -230,8 +250,8 @@ do
                                     cmd=$cmd" --save_logits"
                                 fi
 
-                                if [ "$eval" = "True" ]; then
-                                    cmd=$cmd" --eval --resume $resume"
+                                if [ ! -z "$eval_ckpt" ]; then
+                                    cmd=$cmd" --eval --resume $output_dir"/"$eval_ckpt"
                                 fi
 
                                 if [ ! -z "$resume" ]; then
