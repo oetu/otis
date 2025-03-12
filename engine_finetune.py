Add: option for frequency-based weighted loss; torch.compile();; Update: processing of time series with up to 4 dimensions; default metrics averaging set to macro; conda environment;; Fix: matplotlib (tkinter) error for DDP training

engine_finetune.py
@@ -27,7 +27,10 @@ from sklearn.decomposition import PCA
 
 import wandb
 
+import matplotlib
+matplotlib.use('Agg')           # prevents tkinter error
 import matplotlib.pyplot as plt
+
 import numpy as np
 
 from timm.data import Mixup
@@ -77,7 +80,7 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
         if mixup_fn is not None:
             samples, targets = mixup_fn(samples, targets)
 
-        with torch.cuda.amp.autocast():
+        with torch.amp.autocast(device_type="cuda"):
             outputs = model(samples, pos_embed_y) * targets_mask
             loss = criterion(outputs, targets)
 
@@ -121,13 +124,13 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
     training_stats = {k: meter.global_avg for k, meter in metric_logger.meters.items()}
     if args.downstream_task == 'classification':
         labels_onehot = torch.nn.functional.one_hot(labels, num_classes=-1)             # (B, num_classes)
-        f1 = 100*f1_score(y_true=labels, y_pred=logits.argmax(dim=-1), average="weighted")
+        f1 = 100*f1_score(y_true=labels, y_pred=logits.argmax(dim=-1), average="macro")                 # weighted
         precision = 100*precision_score(y_true=labels, y_pred=logits.argmax(dim=-1), average="macro")   # macro
         recall = 100*recall_score(y_true=labels, y_pred=logits.argmax(dim=-1), average="macro")         # macro
         acc = 100*accuracy_score(y_true=labels, y_pred=logits.argmax(dim=-1))
         acc_balanced = 100*balanced_accuracy_score(y_true=labels, y_pred=logits.argmax(dim=-1))
         if args.nb_classes > 2:
-            auc = 100*roc_auc_score(y_true=labels, y_score=probs, average="macro", multi_class="ovo")
+            auc = 100*roc_auc_score(y_true=labels, y_score=probs, average="macro", multi_class="ovr")
         else:
             auc = 100*roc_auc_score(y_true=labels, y_score=probs[:, 1], average="macro")
         auprc = 100*average_precision_score(y_true=labels_onehot, y_score=probs, average="macro")
@@ -247,7 +250,7 @@ def evaluate(data_loader, model, device, epoch, log_writer=None, args=None):
             target_mask = target_mask.unsqueeze(dim=-1).repeat(1, args.nb_classes)
 
         # compute output
-        with torch.cuda.amp.autocast():
+        with torch.amp.autocast(device_type="cuda"):
             embedding = model.forward_features(images, pos_embed_y)
             output = model.forward_head(embedding)
             output = output * target_mask
@@ -293,13 +296,13 @@ def evaluate(data_loader, model, device, epoch, log_writer=None, args=None):
     test_stats = {k: meter.global_avg for k, meter in metric_logger.meters.items()}
     if args.downstream_task == 'classification':
         labels_onehot = torch.nn.functional.one_hot(labels, num_classes=-1)                 # (B, num_classes)
-        f1 = 100*f1_score(y_true=labels, y_pred=logits.argmax(dim=-1), average="weighted")
-        precision = 100*precision_score(y_true=labels, y_pred=logits.argmax(dim=-1), average="macro")    # macro
-        recall = 100*recall_score(y_true=labels, y_pred=logits.argmax(dim=-1), average="macro")          # macro
+        f1 = 100*f1_score(y_true=labels, y_pred=logits.argmax(dim=-1), average="macro")                 # weighted
+        precision = 100*precision_score(y_true=labels, y_pred=logits.argmax(dim=-1), average="macro")   # macro
+        recall = 100*recall_score(y_true=labels, y_pred=logits.argmax(dim=-1), average="macro")         # macro
         acc = 100*accuracy_score(y_true=labels, y_pred=logits.argmax(dim=-1))
         acc_balanced = 100*balanced_accuracy_score(y_true=labels, y_pred=logits.argmax(dim=-1))
         if args.nb_classes > 2:
-            auc = 100*roc_auc_score(y_true=labels, y_score=probs, average="macro", multi_class="ovo")
+            auc = 100*roc_auc_score(y_true=labels, y_score=probs, average="macro", multi_class="ovr")
         else:
             auc = 100*roc_auc_score(y_true=labels, y_score=probs[:, 1], average="macro")
         auprc = 100*average_precision_score(y_true=labels_onehot, y_score=probs, average="macro")
@@ -445,9 +448,7 @@ def evaluate(data_loader, model, device, epoch, log_writer=None, args=None):
             plt.yticks([])
 
             plt.legend(loc="lower left")
-
             plt.tight_layout()
-
             test_history["PCA of val embeddings"] = wandb.Image(fig)
             plt.close('all')
     

engine_pretrain.py
@@ -27,7 +27,7 @@ import util.lr_sched as lr_sched
 import util.statistics as statistics
 
 import matplotlib
-matplotlib.use('Agg')
+matplotlib.use('Agg')           # prevents tkinter error
 import matplotlib.pyplot as plt
 
 from sklearn.metrics import f1_score, accuracy_score, balanced_accuracy_score
@@ -68,7 +68,7 @@ def train_one_epoch(model: torch.nn.Module,
         pos_embed_y = pos_embed_y.to(device, non_blocking=True)
 
         # compute model prediction
-        with torch.cuda.amp.autocast():
+        with torch.amp.autocast(device_type="cuda"):
             loss, ncc, cos_sim, cos_sim_embed, z_std, samples_hat, mask, _ = model(samples, 
                                                                                    attn_mask, 
                                                                                    pos_embed_y, 
@@ -296,7 +296,7 @@ def evaluate_online(estimator, model, device, train_dataloader, val_dataloader, 
         train_labels.append(label.to(device, non_blocking=True))
         pos_embed_y = pos_embed_y.to(device, non_blocking=True)
 
-        with torch.cuda.amp.autocast():
+        with torch.amp.autocast(device_type="cuda"):
             train_embeddings.append(model.forward_encoder_all_patches(data, pos_embed_y))
 
     train_embeddings = torch.cat(train_embeddings, dim=0)[:, 1:, :].mean(dim=1) # globally average pooled token
@@ -308,13 +308,16 @@ def evaluate_online(estimator, model, device, train_dataloader, val_dataloader, 
     
     if args.online_evaluation_task == "classification":
         train_probs = torch.tensor(estimator.predict_proba(train_embeddings), dtype=torch.float16)
-        classifier_f1_train = f1_score(y_true=train_labels, y_pred=train_probs.argmax(dim=-1), average="weighted")
-        classifier_precision_train = precision_score(y_true=train_labels, y_pred=train_probs.argmax(dim=-1), average="weighted")
-        classifier_recall_train = recall_score(y_true=train_labels, y_pred=train_probs.argmax(dim=-1), average="weighted")
+        classifier_f1_train = f1_score(y_true=train_labels, y_pred=train_probs.argmax(dim=-1), average="macro")
+        classifier_precision_train = precision_score(y_true=train_labels, y_pred=train_probs.argmax(dim=-1), average="macro")
+        classifier_recall_train = recall_score(y_true=train_labels, y_pred=train_probs.argmax(dim=-1), average="macro")
         classifier_acc_train = accuracy_score(y_true=train_labels, y_pred=train_probs.argmax(dim=-1))
         classifier_acc_balanced_train = balanced_accuracy_score(y_true=train_labels, y_pred=train_probs.argmax(dim=-1))
-        classifier_auc_train = roc_auc_score(y_true=torch.nn.functional.one_hot(train_labels, num_classes=-1), y_score=train_probs, average="weighted")
-        classifier_auprc_train = average_precision_score(y_true=torch.nn.functional.one_hot(train_labels, num_classes=-1), y_score=train_probs, average="weighted")
+        if args.online_num_classes > 2:
+            classifier_auc_train = roc_auc_score(y_true=train_labels, y_score=train_probs, average="macro", multi_class="ovr")
+        else:
+            classifier_auc_train = roc_auc_score(y_true=train_labels, y_score=train_probs[:, 1], average="macro")
+        classifier_auprc_train = average_precision_score(y_true=torch.nn.functional.one_hot(train_labels, num_classes=args.online_num_classes), y_score=train_probs, average="macro")
     elif args.online_evaluation_task == "regression":
         train_preds = torch.tensor(estimator.predict(train_embeddings), dtype=torch.float16)
         classifier_rmse_train = np.float64(root_mean_squared_error(train_preds, train_labels, multioutput="raw_values"))
@@ -331,7 +334,7 @@ def evaluate_online(estimator, model, device, train_dataloader, val_dataloader, 
         val_labels.append(label.to(device, non_blocking=True))
         pos_embed_y = pos_embed_y.to(device, non_blocking=True)
 
-        with torch.cuda.amp.autocast():
+        with torch.amp.autocast(device_type="cuda"):
             val_embeddings.append(model.forward_encoder_all_patches(data, pos_embed_y))
 
     val_embeddings = torch.cat(val_embeddings, dim=0)[:, 1:, :].mean(dim=1) # globally average pooled token
@@ -341,13 +344,16 @@ def evaluate_online(estimator, model, device, train_dataloader, val_dataloader, 
     
     if args.online_evaluation_task == "classification":
         val_probs = torch.tensor(estimator.predict_proba(val_embeddings), dtype=torch.float16)
-        classifier_f1_val = f1_score(y_true=val_labels, y_pred=val_probs.argmax(dim=-1), average="weighted")
-        classifier_precision_val = precision_score(y_true=val_labels, y_pred=val_probs.argmax(dim=-1), average="weighted")
-        classifier_recall_val = recall_score(y_true=val_labels, y_pred=val_probs.argmax(dim=-1), average="weighted")
+        classifier_f1_val = f1_score(y_true=val_labels, y_pred=val_probs.argmax(dim=-1), average="macro")
+        classifier_precision_val = precision_score(y_true=val_labels, y_pred=val_probs.argmax(dim=-1), average="macro")
+        classifier_recall_val = recall_score(y_true=val_labels, y_pred=val_probs.argmax(dim=-1), average="macro")
         classifier_acc_val = accuracy_score(y_true=val_labels, y_pred=val_probs.argmax(dim=-1))
         classifier_acc_balanced_val = balanced_accuracy_score(y_true=val_labels, y_pred=val_probs.argmax(dim=-1))
-        classifier_auc_val = roc_auc_score(y_true=torch.nn.functional.one_hot(val_labels, num_classes=-1), y_score=val_probs, average="weighted")
-        classifier_auprc_val = average_precision_score(y_true=torch.nn.functional.one_hot(val_labels, num_classes=-1), y_score=val_probs, average="weighted")
+        if args.online_num_classes > 2:
+            classifier_auc_val = roc_auc_score(y_true=val_labels, y_score=val_probs, average="macro", multi_class="ovr")
+        else:
+            classifier_auc_val = roc_auc_score(y_true=val_labels, y_score=val_probs[:, 1], average="macro")
+        classifier_auprc_val = average_precision_score(y_true=torch.nn.functional.one_hot(val_labels, num_classes=args.online_num_classes), y_score=val_probs, average="macro")
     elif args.online_evaluation_task == "regression":
         val_preds = torch.tensor(estimator.predict(val_embeddings), dtype=torch.float16)
         classifier_rmse_val = np.float64(root_mean_squared_error(val_preds, val_labels, multioutput="raw_values"))
@@ -412,7 +418,7 @@ def evaluate(data_loader, model, device, epoch, log_writer=None, args=None):
 
         domain = batch[3]
 
-        with torch.cuda.amp.autocast():
+        with torch.amp.autocast(device_type="cuda"):
             loss, ncc, cos_sim, cos_sim_embed, z_std, samples_hat, mask, latent = model(samples, 
                                                                                         attn_mask, 
                                                                                         pos_embed_y, 

main_forecast.py
@@ -363,17 +363,20 @@ def main(args):
 
     new_patch_size = False
     if args.finetune and not args.eval:
-        checkpoint = torch.load(args.finetune, map_location='cpu')
+        checkpoint = torch.load(args.finetune, map_location='cpu', weights_only=False)
 
         print("Load pretrained checkpoint from: %s" % args.finetune)
         checkpoint_model = checkpoint['model']
 
         # check if new and old patch_size match
+        nb_channels_ckpt = checkpoint_model['patch_embed.proj.weight'].shape[-3]
+        nb_channels_model = args.input_size[0]
+
         checkpoint_patch_size = checkpoint_model['patch_embed.proj.weight'].shape[-2:]
         patch_height_ckpt, patch_width_ckpt = checkpoint_patch_size[0], checkpoint_patch_size[1]
         patch_height_model, patch_width_model = args.patch_size[0], args.patch_size[1]
 
-        if patch_height_ckpt != patch_height_model or patch_width_ckpt != patch_width_model:
+        if nb_channels_ckpt != nb_channels_model or patch_height_ckpt != patch_height_model or patch_width_ckpt != patch_width_model:
             new_patch_size = True
             # initialize new patch_embed
             for key in ["patch_embed.proj.weight", "patch_embed.proj.bias", 
@@ -567,7 +570,7 @@ def main(args):
         trainable_params.append(f"mask_token")
 
     if args.compile:
-        model = torch.compile(model)
+        model = torch.compile(model, backend="inductor", mode="reduce-overhead")
     model.to(device, non_blocking=True)
 
     model_without_ddp = model
@@ -750,7 +753,7 @@ def main(args):
     if args.test and misc.is_main_process():
         args.resume = misc.get_best_ckpt(args.output_dir, eval_criterion=args.eval_criterion)
         
-        checkpoint = torch.load(args.resume)
+        checkpoint = torch.load(args.resume, weights_only=False)
         model_without_ddp.load_state_dict(checkpoint['model'])
         print("Run test data on checkpoint model %s" % args.resume)
         

main_linprobe.py
@@ -12,8 +12,6 @@
 import os
 import argparse
 
-import re
-
 import json
 from typing import Tuple
 import numpy as np
@@ -53,6 +51,8 @@ def get_args_parser():
     # Model parameters
     parser.add_argument('--model', default='vit_baseDeep_patchX', type=str, metavar='MODEL',
                         help='Name of model to train (default: vit_baseDeep_patchX)')
+    parser.add_argument('--compile', action='store_true', default=False,
+                        help='Use torch compile')
     
     parser.add_argument('--univariate', action='store_true', default=False,
                         help='Univariate time series analysis (i.e. treat each variate independently)')
@@ -121,6 +121,8 @@ def get_args_parser():
                         help='Early stopping threshold (val has to be worse than (train+delta)) (default: 0)')
 
     # Criterion parameters
+    parser.add_argument('--weighted_loss', action='store_true', default=False,
+                        help='Apply inverse frequency weighted loss (default: False)')
     parser.add_argument('--smoothing', type=float, default=0.1,
                         help='Label smoothing (default: 0.1)')
 
@@ -257,6 +259,7 @@ def main(args):
                                       labels_path=args.labels_path, 
                                       labels_mask_path=args.labels_mask_path, 
                                       downstream_task=args.downstream_task, 
+                                      weighted_loss=args.weighted_loss,
                                       univariate=args.univariate,
                                       train=True, 
                                       N_val=1,
@@ -281,9 +284,6 @@ def main(args):
                                         test=True,
                                         N_val=1,
                                         args=args)
-
-    # train balanced
-    # class_weights = 2.0 / (2.0 * torch.Tensor([1.0, 1.0])) # total_nb_samples / (nb_classes * samples_per_class)
 
     print("Training set size: ", len(dataset_train))
     print("Validation set size: ", len(dataset_val))
@@ -384,7 +384,7 @@ def main(args):
 
     new_patch_size = False
     if args.finetune and not args.eval:
-        checkpoint = torch.load(args.finetune, map_location='cpu')
+        checkpoint = torch.load(args.finetune, map_location='cpu', weights_only=False)
 
         print("Load pretrained checkpoint from: %s" % args.finetune)
         checkpoint_model = checkpoint['model']
@@ -395,11 +395,14 @@ def main(args):
                 del checkpoint_model[k]
 
         # check if new and old patch_size match
+        nb_channels_ckpt = checkpoint_model['patch_embed.proj.weight'].shape[-3]
+        nb_channels_model = args.input_size[0]
+
         checkpoint_patch_size = checkpoint_model['patch_embed.proj.weight'].shape[-2:]
         patch_height_ckpt, patch_width_ckpt = checkpoint_patch_size[0], checkpoint_patch_size[1]
         patch_height_model, patch_width_model = args.patch_size[0], args.patch_size[1]
 
-        if patch_height_ckpt != patch_height_model or patch_width_ckpt != patch_width_model:
+        if nb_channels_ckpt != nb_channels_model or patch_height_ckpt != patch_height_model or patch_width_ckpt != patch_width_model:
             new_patch_size = True
             # initialize new patch_embed
             for key in ["patch_embed.proj.weight", "patch_embed.proj.bias", 
@@ -513,7 +516,7 @@ def main(args):
             args.resume = "/".join(sub_strings[:-1]) + "/checkpoint-" + str(0) + ".pth"
 
         # load pos_embed_x from checkpoint
-        checkpoint = torch.load(args.resume, map_location='cpu')
+        checkpoint = torch.load(args.resume, map_location='cpu', weights_only=False)
         checkpoint_model = checkpoint['model']
         interpolate_pos_embed_x(model, checkpoint_model)
 
@@ -529,6 +532,8 @@ def main(args):
         dataset_val.set_domain_offsets(checkpoint["domain_offsets"])
         print(f"New domain_offsets: {dataset_train.offsets}")
     
+    if args.compile:
+        model = torch.compile(model, backend="inductor", mode="reduce-overhead")
     model.to(device, non_blocking=True)
 
     model_without_ddp = model
@@ -568,8 +573,11 @@ def main(args):
     print(optimizer)
     loss_scaler = NativeScaler()
 
-    # class_weights = class_weights.to(device=device, non_blocking=True)
     class_weights = None
+    print(dataset_train.class_weights)
+    if dataset_train.class_weights is not None:
+        class_weights = dataset_train.class_weights.to(device=device, non_blocking=True)
+    
     if args.downstream_task == 'regression':
         criterion = torch.nn.MSELoss()
     elif args.smoothing > 0.:
@@ -634,7 +642,7 @@ def main(args):
 
         if args.eval_criterion == "epoch":
             if args.downstream_task == 'classification':
-                test_stats['avg'] = (test_stats['acc_balanced'] + test_stats['f1'] + test_stats['cohen']) / 3
+                test_stats['avg'] = (test_stats['auroc'] + test_stats['acc_balanced'] + test_stats['f1'] + test_stats['cohen']) / 4
             elif args.downstream_task == 'regression':
                 test_stats['avg'] = (test_stats['pcc'] + test_stats['r2']) / 2
             if args.output_dir:
@@ -676,7 +684,7 @@ def main(args):
                     mode="decreasing", domains=dataset_train.domains, domain_offsets=dataset_train.offsets)
         else:
             if args.downstream_task == 'classification':
-                test_stats['avg'] = (test_stats['acc_balanced'] + test_stats['f1'] + test_stats['cohen']) / 3
+                test_stats['avg'] = (test_stats['auroc'] + test_stats['acc_balanced'] + test_stats['f1'] + test_stats['cohen']) / 4
             elif args.downstream_task == 'regression':
                 test_stats['avg'] = (test_stats['pcc'] + test_stats['r2']) / 2
             
@@ -766,7 +774,7 @@ def main(args):
     if args.test and misc.is_main_process():
         args.resume = misc.get_best_ckpt(args.output_dir, eval_criterion=args.eval_criterion)
         
-        checkpoint = torch.load(args.resume)
+        checkpoint = torch.load(args.resume, weights_only=False)
         model_without_ddp.load_state_dict(checkpoint['model'])
         print("Run test data on checkpoint model %s" % args.resume)
         

main_pretrain.py
@@ -398,17 +398,20 @@ def main(args):
 
     new_patch_size = False
     if args.pretrained_encoder:
-        checkpoint = torch.load(args.pretrained_encoder, map_location='cpu')
+        checkpoint = torch.load(args.pretrained_encoder, map_location='cpu', weights_only=False)
 
         print("Load pretrained encoder from: %s" % args.pretrained_encoder)
         checkpoint_model = checkpoint['model']
 
         # check if new and old patch_size match
+        nb_channels_ckpt = checkpoint_model['patch_embed.proj.weight'].shape[-3]
+        nb_channels_model = args.input_size[0]
+
         checkpoint_patch_size = checkpoint_model['patch_embed.proj.weight'].shape[-2:]
         patch_height_ckpt, patch_width_ckpt = checkpoint_patch_size[0], checkpoint_patch_size[1]
         patch_height_model, patch_width_model = args.patch_size[0], args.patch_size[1]
 
-        if patch_height_ckpt != patch_height_model or patch_width_ckpt != patch_width_model:
+        if nb_channels_ckpt != nb_channels_model or patch_height_ckpt != patch_height_model or patch_width_ckpt != patch_width_model:
             new_patch_size = True
             # initialize new patch_embed module
             for key in ["patch_embed.proj.weight", "patch_embed.proj.bias", 
@@ -505,7 +508,7 @@ def main(args):
     print(skip_list)
 
     if args.compile:
-        model = torch.compile(model)
+        model = torch.compile(model, backend="inductor", mode="reduce-overhead")
     model.to(device, non_blocking=True)
 
     n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)

util/augmentations.py
@@ -195,7 +195,13 @@ class CropResizing(object):
             if self.fixed_resize_len is None or self.fixed_resize_len > max_nb_time_steps:
                 resize_len = max_nb_time_steps
 
-            cropped_sample = torch.nn.functional.interpolate(cropped_sample, size=resize_len, mode="linear")
+            if cropped_sample.dim() > 3:
+                N, C, V, T = cropped_sample.shape
+                cropped_sample = cropped_sample.flatten(start_dim=0, end_dim=1)
+                cropped_sample = torch.nn.functional.interpolate(cropped_sample, size=resize_len, mode="linear")
+                cropped_sample = cropped_sample.view(N, C, V, -1)
+            else:
+                cropped_sample = torch.nn.functional.interpolate(cropped_sample, size=resize_len, mode="linear")
 
         return cropped_sample
 

util/dataset.py
@@ -5,14 +5,12 @@
 # LICENSE file in the root directory of this source tree.
 # --------------------------------------------------------
 from typing import Any, Tuple, Dict
-import random
 
 import torch
 from torch.utils.data import Dataset
 from torchvision import transforms
 
 import util.augmentations as augmentations
-import util.transformations as transformations
 
 
 class TimeSeriesDataset(Dataset):
@@ -20,7 +18,7 @@ class TimeSeriesDataset(Dataset):
     Dataset for multi-domain time series analysis.
     """
     def __init__(self, data_path:str, labels_path:str=None, labels_mask_path:str=None, 
-                 downstream_task:str=None, 
+                 downstream_task:str=None, weighted_loss:bool=False,
                  domain_offsets:Dict=None, domain_agnostic:str=False, 
                  univariate:bool=False, 
                  train:bool=False, 
@@ -41,11 +39,16 @@ class TimeSeriesDataset(Dataset):
             train: training or validation / test
             N_val: nb of chunks to validate / test
         """
-        data = torch.load(data_path, map_location=torch.device('cpu')) # load to ram
+        # data defined as list with tuples (modality:str, sample:torch.tensor())
+        data = torch.load(data_path, map_location=torch.device('cpu'), weights_only=False) # load to ram
 
-        # .unsqueeze(0) to add auxiliary channel (similar to rgb in imgs)
-        domain = [(sample[0], sample[1].unsqueeze(0).shape) for sample in data]
-        data = [sample[1].unsqueeze(0) for sample in data]
+        if data[0][1].dim() == 3:
+            domain = [(sample[0], sample[1].shape) for sample in data] # domain_shape : (C, V, T)
+            data = [sample[1][None, ...] for sample in data] # data_shape : (1, C, V, T)
+        else:
+            # [None, ] to add auxiliary channel (C) dimension (similar to rgb in imgs)
+            domain = [(sample[0], sample[1][None, ...].shape) for sample in data] # domain_shape : (C, V, T)
+            data = [sample[1][None, None, ...] for sample in data] # data_shape : (1, C, V, T)
 
         self.univariate = univariate
         self.domain_agnostic = True if self.univariate else domain_agnostic
@@ -74,17 +77,24 @@ class TimeSeriesDataset(Dataset):
 
         self.data = data
 
+        # one-hot-encdoded labels defined as torch.tensor()
         if labels_path:
-            self.labels = torch.load(labels_path, map_location=torch.device('cpu')) # load to ram
+            self.labels = torch.load(labels_path, map_location=torch.device('cpu'), weights_only=False) # load to ram
         else:
             self.labels = torch.zeros(size=(len(self.data), ))
 
         if labels_mask_path:
-            self.labels_mask = torch.load(labels_mask_path, map_location=torch.device('cpu')) # load to ram
+            self.labels_mask = torch.load(labels_mask_path, map_location=torch.device('cpu'), weights_only=False) # load to ram
         else:
             self.labels_mask = torch.ones_like(self.labels)
 
         self.downstream_task = downstream_task
+        
+        self.weighted_loss = weighted_loss
+        self.class_weights = None
+        if self.downstream_task == "classification" and weighted_loss == True:
+            self.class_weights = torch.sqrt(len(self.labels) / self.labels.argmax(-1).unique(return_counts=True)[-1])
+
         self.train = train 
         self.test = test
         self.N_val = N_val
@@ -100,18 +110,18 @@ class TimeSeriesDataset(Dataset):
 
     def __getitem__(self, idx) -> Tuple[Any, Any]:
         """return a sample from the dataset at index idx"""
-        # (1, V, T)
+        # (1, C, V, T)
         data = self.data[idx]
         if self.univariate:
             # transform a multivariate time series with V variates into V univariate time series
-            # (V, 1, T)
-            data = data.permute(1, 0, 2)
+            # (V, C, 1, T)
+            data = data.permute(2, 1, 0, 3)
         
         # number of samples to process
         N = data.shape[0]
 
-        # (N, V, T) if multivariate analysis, 
-        # (N*V, 1, T) else
+        # (N=N_val,   C, V, T)   if multivariate analysis, 
+        # (N=N_val*V, C, 1, T) else
         if self.train == False:
             # validate / test on more than one chunk per sample
             # Calculate number of overlapping chunks
@@ -128,6 +138,7 @@ class TimeSeriesDataset(Dataset):
             data_chunks = [ data[..., i*stride:(i*stride+self.args.time_steps)] for i in range(N_val) ]
 
             # Concatenate chunks
+            # (N, C, x, T), where x either V or 1
             data = torch.cat(data_chunks, dim=0)
         else:
             N_val = max(data.shape[-1] - self.args.time_steps + 1, 1)
@@ -149,6 +160,7 @@ class TimeSeriesDataset(Dataset):
             data_chunks = [ transform(data) for i in range(N_val) ]
 
             # Concatenate chunks
+            # (N, C, x, T), where x either V or 1
             data = torch.cat(data_chunks, dim=0)
 
         if self.downstream_task == 'regression':
@@ -184,24 +196,24 @@ class TimeSeriesDataset(Dataset):
             grid_width = grid_width + 1
 
         # Zero pad the input data to the largest shape
-        # (B, 1, V_max, T_max)
+        # (B, C, V_max, T_max)
         data = [torch.nn.functional.pad(data.unsqueeze(0), 
                                         pad=(0, int(max_timesteps - data.shape[-1]), 0, int(max_variates - data.shape[-2])), 
                                         mode="constant", value=0) for sample in batch for data in sample[0]]
-        data = torch.stack(data, dim=0)
+        data = torch.cat(data, dim=0)
 
         # Create the attention mask 
         # (B, V'_max, T'_max), with V'_max=V_max/p, T'_max=T_max/p
-        attn_mask = [torch.nn.functional.pad(torch.ones(size=(grid_height[idx], grid_width[idx])), 
-                                                pad=(0, int(grid_width.max() - grid_width[idx]), 0, int(grid_height.max() - grid_height[idx])), 
-                                                mode="constant", value=0) for idx, sample in enumerate(batch) for data in sample[0]]
+        attn_mask = [torch.nn.functional.pad(torch.ones(size=(grid_height[idx], grid_width[idx])),
+                                             pad=(0, int(grid_width.max() - grid_width[idx]), 0, int(grid_height.max() - grid_height[idx])),
+                                             mode="constant", value=0) for idx, sample in enumerate(batch) for i in range(len(sample[0]))]
         attn_mask = torch.stack(attn_mask, dim=0)
         
         # Create the pos embedding Y
         # (B, V'_max, T'_max)
         pos_embed_y = [torch.nn.functional.pad(torch.arange(grid_height[idx]).view(-1, 1).repeat(1, grid_width[idx]) + 1 + sample[4], 
                                                 pad=(0, int(grid_width.max() - grid_width[idx]), 0, int(grid_height.max() - grid_height[idx])), 
-                                                mode="constant", value=0) for idx, sample in enumerate(batch) for data in sample[0]]
+                                                mode="constant", value=0) for idx, sample in enumerate(batch) for i in range(len(sample[0]))]
         pos_embed_y = torch.stack(pos_embed_y, dim=0)
 
         domain = [domain for sample in batch for domain in sample[5]]
@@ -210,8 +222,8 @@ class TimeSeriesDataset(Dataset):
     
     @staticmethod
     def collate_fn_ft(batch):
-        # (B, 1, V, T)
-        data = torch.stack([data.unsqueeze(0) for sample in batch for data in sample[0]], dim=0)
+        # (B, C, V, T)
+        data = torch.cat([data.unsqueeze(0) for sample in batch for data in sample[0]], dim=0)
         # (B, 1)
         label = torch.stack([label for sample in batch for label in sample[1]], dim=0)
         # (B, 1)
@@ -221,6 +233,6 @@ class TimeSeriesDataset(Dataset):
         grid_height = batch[0][0].shape[-2] // batch[0][3][-2]
         # (B, V', T'), V'=V/p and T'=T/q
         pos_embed_y = torch.arange(grid_height).view(-1, 1).repeat(len(batch), 1, grid_width) + 1 + batch[0][4]
-        pos_embed_y = torch.stack([pos_embed_y[idx] for idx, sample in enumerate(batch) for data in sample[0]], dim=0)
+        pos_embed_y = torch.stack([pos_embed_y[idx] for idx, sample in enumerate(batch) for i in range(len(sample[0]))], dim=0)
 
         return data, label, label_mask, torch.LongTensor(pos_embed_y)

util/misc.py
@@ -266,7 +266,7 @@ class NativeScalerWithGradNormCount:
     state_dict_key = "amp_scaler"
 
     def __init__(self):
-        self._scaler = torch.cuda.amp.GradScaler()
+        self._scaler = torch.amp.GradScaler("cuda")
 
     def __call__(self, loss, optimizer, clip_grad=None, parameters=None, create_graph=False, update_grad=True):
         self._scaler.scale(loss).backward(create_graph=create_graph)
@@ -395,7 +395,7 @@ def load_model(args, model_without_ddp, optimizer, loss_scaler):
             checkpoint = torch.hub.load_state_dict_from_url(
                 args.resume, map_location='cpu', check_hash=True)
         else:
-            checkpoint = torch.load(args.resume, map_location='cpu')
+            checkpoint = torch.load(args.resume, map_location='cpu', weights_only=False)
         model_without_ddp.load_state_dict(checkpoint['model'])
         print("Resume checkpoint %s" % args.resume)
         if 'optimizer' in checkpoint and 'epoch' in checkpoint and not (hasattr(args, 'eval') and args.eval):

util/plot.py
@@ -7,12 +7,13 @@
 import torch
 import torch.nn.functional as F
 
+import matplotlib
+matplotlib.use('Agg')           # prevents tkinter error
 import matplotlib.pyplot as plt
+
 from matplotlib.collections import LineCollection
 
 import numpy as np
-
-import seaborn as sns
 
 import wandb
 
@@ -84,60 +85,6 @@ def plot_attention(original_signal, attention_map, sample_idx, head_idx=0):
     # Remove y labels of all subplots
     [ax.yaxis.set_visible(False) for ax in axes.ravel()]
 
-    plt.tight_layout()
-
-    fig_attn = wandb.Image(fig)
-    plt.close('all')
-
-    return fig_attn
-
-
-def plot_attention_old(original_signal, attentation_map, idx):
-    """
-    Plot the attention as a heatmap over the signal.
-    One channel of the signal across all heads.
-
-    :input:
-    original_signal (B, C, C_sig, T_sig)
-    attention_map (B, Heads, C_sig*N_(C_sig), C_sig*N_(C_sig))
-    """
-    B, C, C_sig, T_sig = original_signal.shape
-    B, Heads, N, N = attentation_map.shape
-
-    NpC = int((N-1) / C_sig) # N_(C_sig)
-
-    # only for nice visualization 
-    original_signal = (original_signal+0.5*abs(original_signal.min()))
-
-    # EXEMPLARY FOR THE FIRST SIGNAL CHANNEL
-    # i.e. attention of all tokens (N-1, ignore cls token) to the ones of the first signal channel (N_(C_sig))
-    
-    # (B, Heads, N_(C_sig), N-1)
-    attentation_map = attentation_map[:, :, 1:(1+NpC), 1:] # ignore the cls token
-    # (B, Heads, N_(C_sig))
-    attentation_map = attentation_map.mean(dim=-1)
-    attentation_map = F.normalize(attentation_map, dim=-1)
-    attentation_map = attentation_map.softmax(dim=-1)
-    # (B, Heads, T_sig)
-    attentation_map = F.interpolate(attentation_map, size=T_sig, mode='linear')
-
-    # (T_sig)
-    original_signal = original_signal[idx, 0, 0].cpu()
-    # (Heads, T_sig)
-    attentation_map = attentation_map[idx].cpu()
-
-    fig, axes = plt.subplots(nrows=Heads, sharex=True)
-
-    for head in range(0, Heads):
-        axes[head].plot(range(0, original_signal.shape[-1], 1), original_signal, zorder=2) # (2500)
-        sns.heatmap(attentation_map[head, :].unsqueeze(dim=0).repeat(15, 1), linewidth=0.5, # (1, 2500)
-                    alpha=0.3,
-                    zorder=1,
-                    ax=axes[head])
-        axes[head].set_ylim(original_signal.min(), original_signal.max())
-
-    # remove y labels of all subplots
-    [ax.yaxis.set_visible(False) for ax in axes.ravel()]
     plt.tight_layout()
 
     fig_attn = wandb.Image(fig)